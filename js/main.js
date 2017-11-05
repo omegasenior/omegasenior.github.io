c3e3b99ac2afd015c227a0e67b54170fd4e78097
Nuno Filipe Lopes Costa <nflcosta@gmail.com>
2017-11-05 20:42:34 +0000

    new content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,356 @@
-!function(e){"use strict";var i={run:function(){var t=Object.getOwnPropertyNames(i);e.each(t,function(e,t){-1!==t.indexOf("init")&&i[t]()})},initFeature:function(){e("#accordion").on("shown.bs.collapse",function(){e(this).children(".panel").removeClass("active"),e(this).find(".panel-collapse.in").parent().addClass("active")}).on("hidden.bs.collapse",function(){e(this).children(".panel").removeClass("active")})},initHome:function(){new Swiper(".slide_header_home",{pagination:".swiper-pagi-slide",paginationClickable:!0,spaceBetween:30,effect:"fade",loop:!0,autoplay:3e3}),new Swiper(".swiper-services",{slidesPerView:3,pagination:".swiper-pagi-service",paginationClickable:!0,spaceBetween:43,breakpoints:{640:{slidesPerView:1},920:{slidesPerView:2}}}),new Swiper(".swiper-singleday",{slidesPerView:3,nextButton:".swiper-single-next",prevButton:".swiper-single-prev",spaceBetween:30,breakpoints:{640:{slidesPerView:1},920:{slidesPerView:2}}}),new Swiper(".swiper-ft-singleday",{nextButton:".swiper-ft-single-next",prevButton:".swiper-ft-single-prev"}),new Swiper(".swiper-volunteer",{slidesPerView:3,nextButton:".swiper-volen-next",prevButton:".swiper-volen-prev",spaceBetween:30,breakpoints:{640:{slidesPerView:1},992:{slidesPerView:2}}}),new Swiper(".swiper-daily-acitvities",{slidesPerView:1,nextButton:".swiper-acti-next",prevButton:".swiper-acti-prev",spaceBetween:30}),new Swiper(".swiper-testimonial",{slidesPerView:1,autoplay:5e3,pagination:".swiper-pagi-testinmonial",paginationClickable:!0}),new Swiper(".swiper-about",{slidesPerView:6,nextButton:".swiper-about-next",prevButton:".swiper-about-prev",spaceBetween:0,breakpoints:{640:{slidesPerView:2},992:{slidesPerView:3},1170:{slidesPerView:4}}});var t=new Swiper(".swiper-gallery-contract",{spaceBetween:10,direction:"vertical"}),n=new Swiper(".gallery-thumbs",{spaceBetween:3,centeredSlides:!0,slidesPerView:"auto",touchRatio:.2,slideToClickedSlide:!0,direction:"vertical",slidesPerView:4});t.params.control=n,n.params.control=t,i.fixPositionThumbnail(),e(".owl-carousel-about").owlCarousel({items:6,navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],dots:!1,responsiveClass:!0,responsive:{0:{items:2},768:{items:3},992:{items:4,nav:!0},1200:{items:5,nav:!0},1600:{items:6,nav:!0}}}),e(".hs-testimonial").bxSlider({auto:!0,controls:!1,pager:!1}),e("ul.nav li.dropdown").on({mouseenter:function(){e(this).find(".dropdown-menu").stop(!0,!0).delay(200).fadeIn(500)},mouseleave:function(){e(this).find(".dropdown-menu").stop(!0,!0).delay(200).fadeOut(500)}}),i.isnitializeMaps("maps","42.118494","-75.954495"),e(window).on("resize",function(){i.fixHei&&i.hsFixHeight(),i.fixPositionThumbnail(),i.resizeTestiHeight()}),e(window).on("load",function(){i.testimonialMember()})},initMixup:function(){var i=e("#our-gallery");i.mixItUp("isLoaded")||(i.mixItUp(),e(".controls-filter").find(".filter").first().addClass("active"))},initIncremental:function(){var i=e(".incremental-counter");0!==i.length&&i.each(function(){e(this).incrementalCounter({digits:"auto"})})},testimonialMember:function(){if(i._testi_member=e("#testi-member"),0!==i._testi_member.length){i.resizeTestiHeight();var t=i._testi_member.find(".testi-author");t.on("click","a.bg-hv-overlay",function(n){n.preventDefault();var s=e(this);i._testi_member.find(".testi-detail").fadeOut(function(){e(this).empty().append(s.children(".author-opinion").html()).fadeIn(),t.find(".author-name").text(s.data("name")).fadeIn()})})}},resizeTestiHeight:function(){i._testi_member&&e(window).width()>768&&i._testi_member.find(".relative-content").css("height",i._testi_member.find(".testi-author").height())},hsFixHeight:function(){var t=e(".hs-equal-hei");0!==t.length&&(t.each(function(){e(window).width()<972?e(this).find(".hs-block-hei").parent().css("height","auto"):e(this).find(".hs-block-hei").parent().css("height",e(this).find(".hs-img-hei").height())}),i.fixHei=!0)},initVoteArticle:function(){var i=e(".hs-rating-bar");0!==i.length&&i.find("a").on({mouseenter:function(){e(this).prevAll("a").andSelf().addClass("active"),e(this).nextAll("a").removeClass("active")},mouseleave:function(){var e=i.find(".voted");e.length?(e.prevAll("a").andSelf().addClass("active"),e.nextAll("a").removeClass("active")):i.find("a").removeClass("active")}})},fixPositionThumbnail:function(){e(".gallery-thumbs").length&&e(".gallery-thumbs").find(".swiper-wrapper").css({transform:"translate3d(0, 0, 0)","-webkit-transform":"translate3d(0, 0, 0)"})},isnitializeMaps:function(e,i,t){if(e=document.getElementById(e)){var n={center:new google.maps.LatLng(i,t),zoom:14,mapTypeId:google.maps.MapTypeId.ROADMAP,scrollwheel:!1,styles:[{stylers:[{hue:"#5fcfff"},{saturation:10},{lightness:20},{gamma:1.51}]}]},s=new google.maps.Map(e,n),a=null,r='<div class="map-marker">';r+="<h3>Happy Seniors Llc.</h3>",r+='<p class="p-desc">',r+='<img src="images/local.jpg" alt="">',r+="<span>12 Brocton Street Johnson City, AZ 332789</span>",r+="</p>",r+='<p class="p-desc">',r+='<img src="images/phone.jpg" alt="">',r+="<span>(012) 345-6789</span>",r+="</p>",r+='<p class="p-desc hours">',r+='<img src="images/hours.jpg" alt="">',r+="<span>Mon-Sat: 7:00am - 9:00pm Sun: 9:00am - 5:00pm</span>",r+="</p>",r+='<div class="marker">',r+='<img src="images/marker.png" alt="">',r+="</div>",r+="</div>",a=new google.maps.InfoWindow;var o=new google.maps.LatLng(i,t);a.setOptions({content:r,position:o,scrollwheel:!1,stylers:[{color:"#5fcfff"}]}),a.open(s)}}};e(document).ready(function(){i.run()}),e(window).on("load",function(){i.hsFixHeight()})}(jQuery);
+(function($) {
+
+  'use strict';
+
+  var App = {
+
+    /**
+     * Run
+     */
+    run: function() {
+      var func = Object.getOwnPropertyNames( App );
+      $.each( func, function( i, v ) {
+        if( v.indexOf('init') !== -1 ){
+          App[v]();
+        }
+      });
+    },
+
+    /**
+     * Feature
+     */
+    initFeature: function() {
+      // accordion js
+      var _accordion = $('#accordion');
+      _accordion.on('shown.bs.collapse', function(){
+        $(this).children('.panel').removeClass('active');
+        $(this).find('.panel-collapse.in').parent().addClass('active');
+      }).on('hidden.bs.collapse', function(){
+        $(this).children('.panel').removeClass('active');
+      });
+    },
+
+    /**
+     * Homepage slide
+     */ 
+    initHome: function() {
+      var swiper = new Swiper('.slide_header_home', {
+        pagination: '.swiper-pagi-slide',
+        paginationClickable: true,
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        autoplay: 3000
+      });
+
+      var swiperService = new Swiper('.swiper-services', {
+          slidesPerView:3,
+          pagination: '.swiper-pagi-service',
+          paginationClickable: true,
+          spaceBetween: 43,
+          breakpoints: {
+            // when window width is <= 480px
+            640: {
+              slidesPerView: 1
+            },
+            // when window width is <= 640px
+            920: {
+              slidesPerView: 2
+            }
+          }
+      });
+
+      var swiper2 = new Swiper('.swiper-singleday', {
+        slidesPerView: 3,
+        nextButton: '.swiper-single-next',
+        prevButton: '.swiper-single-prev',
+        spaceBetween: 30,
+        breakpoints: {
+          // when window width is <= 480px
+          640: {
+            slidesPerView: 1
+          },
+          // when window width is <= 640px
+          920: {
+            slidesPerView: 2
+          }
+        }
+      });
+
+       var swiper2 = new Swiper('.swiper-ft-singleday', {
+        nextButton: '.swiper-ft-single-next',
+        prevButton: '.swiper-ft-single-prev'
+      });
+
+      var swiper3 = new Swiper('.swiper-volunteer', {
+        slidesPerView: 3,
+        nextButton: '.swiper-volen-next',
+        prevButton: '.swiper-volen-prev',
+        spaceBetween: 30,
+        breakpoints: {
+          // when window width is <= 480px
+          640: {
+            slidesPerView: 1
+          },
+          // when window width is <= 640px
+          992: {
+            slidesPerView: 2
+          }
+        }
+      });
+
+      var _daily_swiper = new Swiper('.swiper-daily-acitvities', {
+        slidesPerView: 1,
+        nextButton: '.swiper-acti-next',
+        prevButton: '.swiper-acti-prev',
+        spaceBetween: 30
+      });
+
+      var testimonial = new Swiper('.swiper-testimonial',{
+        slidesPerView: 1,
+        autoplay: 5000,
+        pagination: '.swiper-pagi-testinmonial',
+        paginationClickable: true
+      });
+
+      var swiperService = new Swiper('.swiper-about', {
+          slidesPerView:6,
+          nextButton: '.swiper-about-next',
+          prevButton: '.swiper-about-prev',
+          spaceBetween: 0,
+          breakpoints: {
+            // when window width is <= 480px
+            640: {
+              slidesPerView: 2
+            },
+            // when window width is <= 640px
+            992: {
+              slidesPerView: 3
+            },
+            1170: {
+              slidesPerView: 4
+            },
+
+          }
+      });
+      var galleryTop = new Swiper('.swiper-gallery-contract', {
+          spaceBetween: 10,
+          direction: 'vertical'
+      });
+      var galleryThumbs = new Swiper('.gallery-thumbs', {
+          spaceBetween: 3,
+          centeredSlides: true,
+          slidesPerView: 'auto',
+          touchRatio: 0.2,
+          slideToClickedSlide: true,
+          direction: 'vertical',
+          slidesPerView:4,
+      });
+      galleryTop.params.control = galleryThumbs;
+      galleryThumbs.params.control = galleryTop;
+      App.fixPositionThumbnail();
+
+      // galleryTop.height = 358;
+      $('.owl-carousel-about').owlCarousel({
+        items:6,
+        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        dots:false,
+        responsiveClass:true,
+        responsive:{
+          0:{
+            items:2,
+          },
+          768:{
+            items: 3,
+          },
+          992:{
+            items: 4,
+            nav:true
+          },
+          1200:{
+            items:5,
+            nav:true,
+          },
+          1600:{
+            items:6,
+            nav:true
+          }
+        }
+
+      });
+
+      $('.hs-testimonial').bxSlider({
+        auto: true,
+        controls:false,
+        pager:false
+      });
+
+      // hover menu
+      $("ul.nav li.dropdown").on({
+          mouseenter: function () {
+              $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+          },
+          mouseleave: function () {
+              $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+          }
+      });
+
+
+      // maps
+      App.isnitializeMaps('maps', '42.118494', '-75.954495');
+      // google.setOnLoadCallback( App.initializeMaps('maps', '42.118464', '-75.954465') );
+
+      $(window).on('resize', function(){
+        if( App.fixHei ){
+          App.hsFixHeight();
+        }
+        App.fixPositionThumbnail();
+        App.resizeTestiHeight();
+      });
+      $(window).on('load', function(){
+        App.testimonialMember();
+      });
+    },
+    initMixup: function(){
+      var _mixup = $('#our-gallery');
+      if(! _mixup.mixItUp('isLoaded') ){
+        _mixup.mixItUp();
+        $('.controls-filter').find('.filter').first().addClass('active');
+      }
+    },
+    initIncremental: function(){
+      var _increment = $('.incremental-counter');
+      if( _increment.length === 0 ) return;
+      _increment.each( function(){
+        $(this).incrementalCounter({digits:'auto'});
+      })
+    },
+    testimonialMember: function(){
+      App._testi_member = $('#testi-member');
+      if( App._testi_member.length === 0 ) return;
+      App.resizeTestiHeight();
+      var _testi_author = App._testi_member.find('.testi-author');
+      _testi_author.on('click', 'a.bg-hv-overlay', function(e){
+        e.preventDefault();
+        var _this = $(this);
+        App._testi_member.find('.testi-detail').fadeOut( function(){
+          $(this).empty().append( _this.children('.author-opinion').html() ).fadeIn();
+          _testi_author.find('.author-name').text( _this.data('name' ) ).fadeIn();
+        })
+      });
+    },
+    resizeTestiHeight: function(){
+      if( ! App._testi_member ) return;
+      if( $(window).width() > 768){
+        App._testi_member.find('.relative-content').css('height', App._testi_member.find('.testi-author').height() );
+      }
+    },
+    hsFixHeight:function(){
+      var _element = $('.hs-equal-hei');
+      if( _element.length === 0 ) return;
+      _element.each( function(){
+        if( $(window).width() < 972 ){
+          $(this).find('.hs-block-hei').parent().css('height', 'auto' );
+        }else{
+          $(this).find('.hs-block-hei').parent().css('height', $(this).find('.hs-img-hei').height() );
+        }
+      });
+      App.fixHei = true;
+    },
+    initVoteArticle:function(){
+      var _rating_bar = $('.hs-rating-bar');
+      if( _rating_bar.length === 0 ) return;
+
+      _rating_bar.find('a').on({
+          mouseenter: function () {
+              $(this).prevAll('a').andSelf().addClass('active');
+              $(this).nextAll('a').removeClass('active')
+          },
+          mouseleave: function () {
+              var _voted = _rating_bar.find('.voted');
+              if( _voted.length ){
+                _voted.prevAll('a').andSelf().addClass('active');
+                _voted.nextAll('a').removeClass('active');
+              }else{
+                _rating_bar.find('a').removeClass('active');
+              }
+          }
+      });
+
+    },
+    fixPositionThumbnail: function(){
+      if( $('.gallery-thumbs').length ){
+        $('.gallery-thumbs').find('.swiper-wrapper').css({'transform':'translate3d(0, 0, 0)','-webkit-transform':'translate3d(0, 0, 0)'});
+      }
+    },
+    isnitializeMaps: function(element, lat, lng) {
+      var element = document.getElementById(element);
+      if( ! element ) return;
+      var styles = [
+      {
+        stylers: [
+          { hue: "#5fcfff" },
+          { saturation: 10 },
+          { lightness: 20 },
+          { gamma: 1.51 }
+        ]
+      }
+    ];
+      var mapProp = {
+          center: new google.maps.LatLng(lat, lng),
+          zoom: 14,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          scrollwheel:false,
+          styles: styles
+      };
+
+      var map = new google.maps.Map(element, mapProp);
+
+      // var ctString = '<p id="hook">Hello World!</p>';
+      // var infowindow = new google.maps.InfoWindow();
+      // infowindow.setContent(ctString);
+      // infowindow.open(map, marker);
+
+      var infoWindow = null;
+      var ctMaps = '<div class="map-marker">';
+          ctMaps += '<h3>Happy Seniors Llc.</h3>';
+          ctMaps +=  '<p class="p-desc">';
+          ctMaps +=   '<img src="images/local.jpg" alt="">';
+          ctMaps +=    '<span>12 Brocton Street Johnson City, AZ 332789</span>';
+          ctMaps +=  '</p>';
+          ctMaps +=  '<p class="p-desc">';
+          ctMaps +=    '<img src="images/phone.jpg" alt="">';
+          ctMaps +=    '<span>(012) 345-6789</span>';
+          ctMaps +=  '</p>';
+          ctMaps +=  '<p class="p-desc hours">';
+          ctMaps +=   '<img src="images/hours.jpg" alt="">';
+          ctMaps +=    '<span>Mon-Sat: 7:00am - 9:00pm Sun: 9:00am - 5:00pm</span>';
+          ctMaps +=  '</p>';
+          ctMaps +=  '<div class="marker">';
+          ctMaps +=    '<img src="images/marker.png" alt="">';
+          ctMaps +=  '</div>';
+          ctMaps +='</div>';
+
+      infoWindow = new google.maps.InfoWindow();
+      var windowLatLng = new google.maps.LatLng(lat,lng);
+      infoWindow.setOptions({
+          content: ctMaps,
+          position: windowLatLng,
+          scrollwheel:false,
+          stylers: [
+            { color: "#5fcfff" }
+          ]
+      });
+      infoWindow.open(map);
+    }
+
+  };
+
+  $( document ).ready(function() {
+    App.run();
+});
+  $(window).on('load', function(){
+    App.hsFixHeight();
+  });
+
+})(jQuery);
